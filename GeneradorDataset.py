dccb9b5279eaf6796272b4d37e5e68c2a6be013a
valehp <valeeehp@gmail.com>
2021-06-30 22:06:50 -0400

    Add files via upload

---
+++ GeneradorDataset.py
@@ -0,0 +1,133 @@
+import sys, getopt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+import pandas as pd
+import random
+from sklearn.preprocessing import MinMaxScaler
+import argparse
+
+base = "./dataset/"
+
+def shape_data(df, file, h=4):      # toma el df segmentado y lo convierte en una matriz de bools 
+    n = len(df.index)
+    data = []
+    df_ejes = pd.DataFrame()
+
+    for eje in df.columns:
+        print(eje)
+        segs = np.zeros( (n, h), dtype=int )
+
+        for i in range(n):
+            k = df[eje][i]
+            segs[i][k] = 1
+
+        for i in range(segs.shape[1]):
+            df_ejes[eje+str(i)] = segs[:,i]
+        #df_eje.to_csv(base + eje + "_" + file)
+    
+    print(df_ejes.head())
+    df_ejes.to_csv(base + file)
+
+
+def get_distribution(opciones_ejes, distribuciones_ejes, plots, random_ax):
+    if plots: f, axs = plt.subplots( 2, 2, figsize=(15, 15) )
+    i, j = 0, 0
+
+    for k in distribuciones_ejes.keys():
+        if random_ax: dist, r, l = random.choice( opciones_ejes )
+        else: dist, r, l = opciones_ejes[0]
+        distribuciones_ejes[k] = [dist, r, l]
+        if plots:
+            x = np.linspace(r, l, 1000)
+            axs[i][j].plot( x, dist.pdf(x) )
+            axs[i][j].set_title( k )
+            i += 1
+            if i == 2:
+                i = 0
+                j = 1
+
+    if plots: plt.show()
+
+
+
+def get_hist(df, ejes, plots):
+    new_df = pd.DataFrame()
+    order_df = pd.DataFrame()
+    for eje in ejes:
+        print(eje)
+        h, b = np.histogram( df[eje], bins=n_segmentos )
+        segmentos = []
+
+        # Recorrer todos los n datos
+        for i in df.index:
+            # Recorrer los n_segmentos del eje actual
+            for k in range(n_segmentos):
+                x = df[eje][i]
+                if ( k==0 and x>=b[k] and x<=b[k+1] ) or ( x>b[k] and b[k+1]>=x ):
+                    segmentos.append(k)
+                    break
+        
+        new_df[eje] = segmentos
+        aux = list(zip( h, [i for i in range(len(eje))] ))
+        aux.sort(reverse=True)
+        order_df[eje] = [ aux[i][1] for i in range(len((aux))) ]
+    return new_df
+    # new_df    -> df con datos segmentados (0, 1, 2, 3)
+    # order_df  -> df con el histograma
+
+
+def generar(personas, distribuciones, ejes ):
+    df = pd.DataFrame()
+    datos = dict(zip( ejes, [ [] for i in range(len(ejes)) ] ))
+
+    for i in range(personas):
+        for eje in ejes:
+            datos[eje].append( distribuciones[eje][0].rvs() )
+
+    for eje in ejes:
+        df[eje] = ( datos[eje] - np.min(datos[eje]) ) / (np.max(datos[eje]) - np.min(datos[eje]))   # minmax scaler
+
+    return df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generador de datos')
+    parser.add_argument('-n', '--num_personas', type=int, default=10)
+    args = parser.parse_args()
+    print(args)
+
+    # declarar variables importantes
+    n_personas      = args.num_personas
+    n_segmentos     = 4
+    archivo_dataset = "Dataset{}.csv".format(args.num_personas)
+    random_axis     = False     # False -> cada eje tiene distribución normal ; True -> ejes tienen distribución aleatoria (de opciones_ejes)
+    plots           = False     # False -> no grafica ; True -> Grafica los datos
+
+
+    ejes = ['economia', 'diplomacia', 'estado', 'sociedad']
+    opciones_ejes = [
+        [ss.norm(), -4, 4], 
+        [ss.beta(2, 5), 0, 1],
+        [ss.beta(0.5, 0.5), 0, 1],
+        [ss.beta(3, 1.5), 0, 1],
+    ]
+    distribuciones_ejes = {
+        'economia'  : '', 
+        'diplomacia': '',
+        'estado'    : '',
+        'sociedad':  '',
+    }
+
+
+    get_distribution(opciones_ejes, distribuciones_ejes, plots, random_axis)
+    df = generar(n_personas, distribuciones_ejes, ejes)
+    print("df:")
+    print(df.head(), "\n")
+
+    df_segmentos = get_hist(df, ejes, plots)
+    print("\nSegmentos:")
+    print(df_segmentos.head(), "\n")
+
+    print("\nShape data:")
+    shape_data(df_segmentos, archivo_dataset, n_segmentos)
---
+++ cplex_solver.py
@@ -0,0 +1,115 @@
+from docplex.cp.model import CpoModel
+from docplex.mp.model import Model
+import numpy as np
+import pandas as pd
+import argparse
+import os
+import time
+import sys
+
+
+
+EJES = ['economia', 'diplomacia', 'estado', 'sociedad']
+BASE = "./dataset/"
+FILE = "Registro_soluciones.csv"
+
+def get_data(n, file):
+	try:
+		# Obtener dataset y dejar los datos en matriz 3d de dimensiones (4 x n x 4) = (num_ejes x n x num_segmentos)
+		d = pd.read_csv( BASE+file, index_col=0 )
+		assert len(d.index < n), "Dataset muy pequeño!"
+
+	except FileNotFoundError:
+		print("Archivos no encontrados")
+		sys.exit()
+
+	
+	d = d.head(n).to_numpy()
+	o = pd.DataFrame()
+	datos = [0, 0, 0, 0]
+	for i, j in zip( list(range(0, d.shape[1], 4)), list(range(4)) ):
+		datos[j] = d[:, i:i+4]
+		orden = []
+		for jj in range(datos[j].shape[1]):
+			orden.append( np.sum(datos[j][:,jj]) )
+
+		aux = list(zip( orden, list(range(4)) ))
+		aux.sort(reverse=True)
+		o[EJES[j]] = [ aux[k][1] for k in range(len(aux)) ]
+
+	return np.array(datos), o.to_numpy()
+
+
+def modelo(data, orden, n, l, m, h=4, n_axs=4):
+	model = Model()
+
+	# Variables -> matrix X de LxN
+	x = []
+	for j in range(l):
+		x.append( model.integer_var_list(n, 0, 1, name="x[{}]".format(j)) )
+	x = np.array(x)
+
+
+	# Constraints // sujeto a:
+
+	# ------------------------------------------------------------------------- #
+	# 1) Para cada grupo, la cantidad de integrantes no debe superar el máximo m
+	# La suma de cada fila debe ser <= m
+	for j in range(l):
+		model.add_constraint( model.sum([ x[j,i] for i in range(n) ]) <= m )
+
+
+	# ------------------------------------------------------------------------- #
+	# 2) Cada persona pertenece SOLO a UN grupo y ninguna se queda sin grupo
+	# La suma de cada columna debe ser = 1
+	for i in range(n):
+		model.add_constraint( model.sum([ x[j,i] for j in range(l) ]) == 1 )
+
+
+	model.minimize(model.sum([ model.max([ model.scal_prod(x[j,:], data[e, :, orden[k+1,e]]) - model.scal_prod(x[j,:], data[e, :, orden[k,e]]), 0 ]) for k in range(h-1) for e in range(n_axs) for j in range(l) ]))
+
+	# k+1 = orden[k+1, e] ; k = orden[k, e]
+	print("Modelo listo...")
+	inicio = time.time()
+	sol = model.solve()
+	final = time.time()
+
+	if sol:
+		if not FILE in os.listdir(os.getcwd()):
+			df_sol = pd.DataFrame()
+		else:
+			df_sol = pd.read_csv(FILE, index_col=0)
+
+		print("Solución: ", sol.get_objective_value())
+		print("Tiempo: ", final-inicio)
+		df_sol[ "Sol n={}".format(n) ] = sol.get_objective_value()
+		df_sol[ "Time n={}".format(n) ] = final-inicio
+		df_sol.to_csv(FILE)
+		xx = []
+
+		for j in range(l):
+			print("Grupo ", j, ": ", end="")
+			first = True
+			aux = []
+			for i in range(n):
+				if sol.get_value( "x[{}]_{}".format(j,i)) == 1:
+					print("\t", i, end="")
+					#print("Persona {} pertenece al grupo {}".format(i, j))
+			print()
+
+
+
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Formación de grupos colabnorativos usando cplex')
+	parser.add_argument('-n', '--num_personas', type=int, default=10)
+	parser.add_argument('-l', '--num_grupos', type=int, default=5)
+	parser.add_argument('-m', '--max_personas', type=int, default=2)
+	parser.add_argument('-seg', '--num_segmentos', type=int, default=4)	# segmentos = h
+	parser.add_argument('-d', '--dataset_file', type=str, default="Dataset10000.csv")
+	args = parser.parse_args()
+	print(args)
+
+	data, orden = get_data( args.num_personas, args.dataset_file )
+	modelo( data, orden, args.num_personas, args.num_grupos, args.max_personas )
