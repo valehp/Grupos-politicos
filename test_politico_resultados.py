38dae7aaa00f0a36134100ee1d3b434449052a9f
valehp <valeeehp@gmail.com>
2022-07-06 10:45:46 -0400

    actualizacion en script para resultados ideologias

--- test_politico_resultados.py
+++ test_politico_resultados.py
@@ -14,74 +14,80 @@ def calc_scores(score, max):
     return round(aux, 3)
 
 
-EJES = ["economia", "diplomacia", "estado", "sociedad"]
-respuestas = pd.read_csv("TestPolitico.csv")
-efectos = pd.read_csv("EfectosRespuestas.csv", index_col=0)
+def calcular_puntaje(respuestas, cols, efectos, res=False):
+    EJES = ["economia", "diplomacia", "estado", "sociedad"]
+    #respuestas = pd.read_csv("TestPolitico.csv")
+    #efectos = pd.read_csv("EfectosRespuestas.csv", index_col=0)
 
-# Columnas con preguntas respuestas palta
-#respuestas = list(df.columns[9:,])
-resp = {
-    "Totalmente de acuerdo": 1.0,
-    "De acuerdo": 0.5,
-    "Neutral": 0.0,
-    "En desacuerdo": -0.5,
-    "Totalmente en desacuerdo": -1.0,
-}
-suma_efectos = {
-    "economia": 0,      # -> equality
-    "diplomacia": 0,    # -> peace
-    "estado": 0,        # -> liberty
-    "sociedad": 0,      # -> progress
-}
-max_scores = {
-    "economia" : 0,
-    "diplomacia" : 0,
-    "estado" : 0,
-    "sociedad" : 0,
-}
+    # Columnas con preguntas respuestas palta
+    #respuestas = list(df.columns[9:,])
+    resp = {
+        "Totalmente de acuerdo": 1.0,
+        "De acuerdo": 0.5,
+        "Neutral": 0.0,
+        "En desacuerdo": -0.5,
+        "Totalmente en desacuerdo": -1.0,
+    }
+    suma_efectos = {
+        "economia": 0,      # -> equality
+        "diplomacia": 0,    # -> peace
+        "estado": 0,        # -> liberty
+        "sociedad": 0,      # -> progress
+    }
+    max_scores = {
+        "economia" : 0,
+        "diplomacia" : 0,
+        "estado" : 0,
+        "sociedad" : 0,
+    }
 
-for i in range(70):
-    mult = resp[ respuestas.iat[0, i+9] ]
-    for eje in efectos.columns[1:]:
-        suma_efectos[eje] += mult * efectos.at[i, eje] 
-        max_scores[eje] += abs(efectos.at[i, eje])
-
-
-#print("Max scores: ", max_scores)
-#print("suma efectos: ", suma_efectos), "\n"
-
-"""
-- EJE ECONÓMICO:    equality    ----    wealth
-- EJE DIPLOMACIA:   peace       ----    might
-- EJE ESTADO:       liberty     ----    authority
-- EJE SOCIEDAD:     progress    ----    tradition
-
-ORDEN
-- EJE ECONÓMICO:    igualdad    ----    mercado
-- EJE DIPLOMACIA:   nacion      ----    global / peace
-- EJE ESTADO:       libertad    ----    autoridad
-- EJE SOCIEDAD:     tradicion   ----    progreso
+    for i, c in enumerate(cols):
+        if type(respuestas[c]) != str: mult = 0.0
+        else: mult = resp[ respuestas[c] ]
+        for eje in efectos.columns[1:]:
+            suma_efectos[eje] += mult * efectos.at[i, eje] 
+            max_scores[eje] += abs(efectos.at[i, eje])
 
 
-"""
+    #print("Max scores: ", max_scores)
+    #print("suma efectos: ", suma_efectos), "\n"
 
-# Eje económico : igualdad < - > mercado
-igualdad =  calc_scores( suma_efectos["economia"], max_scores["economia"] )
-mercado  =  round(1 - igualdad, 3)
+    """
+    - EJE ECONÓMICO:    equality    ----    wealth
+    - EJE DIPLOMACIA:   peace       ----    might
+    - EJE ESTADO:       liberty     ----    authority
+    - EJE SOCIEDAD:     progress    ----    tradition
 
-# Eje dipomacia : nacion < - > global
-peace  = calc_scores( suma_efectos["diplomacia"], max_scores["diplomacia"] )
-nacion = round(1 - peace, 3)
+    ORDEN
+    - EJE ECONÓMICO:    igualdad    ----    mercado
+    - EJE DIPLOMACIA:   nacion      ----    global / peace
+    - EJE ESTADO:       libertad    ----    autoridad
+    - EJE SOCIEDAD:     tradicion   ----    progreso
 
-# Eje estado : libertad < - > autoridad
-libertad = calc_scores( suma_efectos["estado"], max_scores["estado"] )
-autoridad= round(1 - libertad, 3)
 
-# Eje sociedad : tradicion < - > progreso
-progreso = calc_scores( suma_efectos["sociedad"], max_scores["sociedad"] )
-tradicion= round(1 - progreso, 3)
+    """
 
-print("ECONOMÍA: \t {} \t ---- \t {}".format(igualdad, mercado) )
-print("DIPLOMACIA: \t {} \t ---- \t {}".format(nacion, peace) )
-print("ESTADO: \t {} \t ---- \t {}".format(libertad, autoridad) )
-print("SOCIEDAD: \t {} \t ---- \t {}".format(tradicion, progreso) )
+    # Eje económico : igualdad < - > mercado
+    igualdad =  calc_scores( suma_efectos["economia"], max_scores["economia"] )
+    mercado  =  round(1 - igualdad, 3)
+
+    # Eje dipomacia : nacion < - > global
+    peace  = calc_scores( suma_efectos["diplomacia"], max_scores["diplomacia"] )
+    nacion = round(1 - peace, 3)
+
+    # Eje estado : libertad < - > autoridad
+    libertad = calc_scores( suma_efectos["estado"], max_scores["estado"] )
+    autoridad= round(1 - libertad, 3)
+
+    # Eje sociedad : tradicion < - > progreso
+    progreso = calc_scores( suma_efectos["sociedad"], max_scores["sociedad"] )
+    tradicion= round(1 - progreso, 3)
+
+    if res:
+        print("ECONOMÍA: \t {} \t ---- \t {}".format(igualdad, mercado) )
+        print("DIPLOMACIA: \t {} \t ---- \t {}".format(nacion, peace) )
+        print("ESTADO: \t {} \t ---- \t {}".format(libertad, autoridad) )
+        print("SOCIEDAD: \t {} \t ---- \t {}".format(tradicion, progreso) )
+        print()
+
+    return (igualdad, nacion, libertad, tradicion)
